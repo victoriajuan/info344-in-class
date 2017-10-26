38b6bdfd21bb38509f50b72a1cdb1bdedd7ede20
Dave Stearns <dlsinfo@uw.edu>
2017-10-26 14:58:22 -0700

    end of trie and mutex day

--- mutex/main.go
+++ mutex/main.go
@@ -9,8 +9,8 @@ import (
 
 func client(index int, c *Cache) {
 	for {
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-		key := strconv.Itoa(rand.Intn(10))
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
+		key := strconv.Itoa(rand.Intn(10000))
 		value := key + " value"
 
 		log.Printf("client %d setting %s=%s", index, key, value)
@@ -25,4 +25,9 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	c := NewCache()
 
+	for i := 0; i < 10; i++ {
+		go client(i, c)
+	}
+
+	time.Sleep(time.Minute)
 }
